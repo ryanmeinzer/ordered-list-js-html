2fa0e4b844af870964734229d5c6f5563c68278d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-22 21:32:32 -0800

    added logic to render ordered list given brute force nested array as input

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const div = document.getElementById('unorderedList')
+// creating div to hold ordered list
+const div = document.getElementById('orderedList')
 
-div.innerHTML = "Hi"
+// starting with brute force nested array
+let headings = ['H1 All About Birds', ['H2 Kinds of Birds', ['H3 The Finch', 'H3 The Swan']], ['H2 Habitats', ['H3 Wetlands']]]
 
-let headings = ['H1 All About Birds', 'H2 Kinds of Birds', 'H3 The Finch', 'H3 The Swan', 'H2 Habitats', 'H3 Wetlands']
-
-
-function toOutline(headings) {
+// transforming array into ordered list
+function toOutline(root, headings) {
+    // create ordered list
+    let ol = document.createElement('ol')
+    // instantiate list
+    let li
+    // make root the ordered list
+    root.appendChild(ol)
+    // for each heading
+    headings.forEach(function (item) {
+        // if it's an array  (ie a list)
+        if (Array.isArray(item)) {
+            // add items to it's list
+            toOutline(li, item)
+            // return
+            return
+        }
+        // create list for DOM
+        li = document.createElement('li')
+        // add children items to respective lists
+        li.appendChild(document.createTextNode(item))
+        // add all to ordered list
+        ol.appendChild(li)
+    })
 }
+// call function to render DOM into div with headings
+toOutline(div, headings)
