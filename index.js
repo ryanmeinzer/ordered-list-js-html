80973108f31a61586a08a6a04ff9a1c597afeff7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-22 20:58:43 -0800

    setting up divs and logic for rendering

---
+++ index.js
@@ -0,0 +1,9 @@
+const div = document.getElementById('unorderedList')
+
+div.innerHTML = "Hi"
+
+let headings = ['H1 All About Birds', 'H2 Kinds of Birds', 'H3 The Finch', 'H3 The Swan', 'H2 Habitats', 'H3 Wetlands']
+
+
+function toOutline(headings) {
+}
